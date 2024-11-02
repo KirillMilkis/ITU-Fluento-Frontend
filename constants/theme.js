cce15ccf7de553e43510841d2ce9b35ee6fb25b2
Kirill Kurakov <kirillkurakov8@gmail.com>
2024-11-02 15:42:08 +0100

    added themes

---
+++ constants/theme.js
@@ -0,0 +1,42 @@
+import {Demensions} from 'react-native';
+const {width, height} = Dimensions.get('window');
+
+export const COLORS = {
+    primary: "#FFB703",
+    secondary: "#8ECAE6",
+    tertiary: "#FB8500",
+
+    gray1 : "#F0F0F0",
+    gray2 : "#CDCDCD",
+
+    green: "#39C01E",
+    lightGreen: "#C3FFA8",
+    black: "#000000",
+    white: "#FFFFFF",
+
+    tabPrimary: "#023047",
+    tabSecondary: "#219EBC",
+
+}
+
+
+const SIZES = {
+    base: 8,
+    font: 14,
+    radius: 12,
+    padding: 24,
+
+    h1: 30,
+    h2: 22,
+    h3: 16,
+    h4: 14,
+    h5: 12,
+
+    body1: 30,
+    body2: 22,
+    body3: 16,
+    body4: 14,
+    body5: 12,
+    height,
+    width,
+}
